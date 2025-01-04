32e66957329c8e62c9fcc5409ff49a4d022e38fb
Yaklede <chh78151@naver.com>
2025-01-04 20:22:13 +0900

    feature: project initalize

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("kapt") version "1.9.25"
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+group = "io.github.Yaklede"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+
+    //reflection
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    //junit
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    //assertj
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
